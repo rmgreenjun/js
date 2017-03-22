686e87533455c5c399a9e2826765b0b0ef5c0bef
Rich Green <rmgreenjun@gmail.com>
2017-03-22 17:52:07 -0400

    initial commit

---
+++ udemy/javascriptTheBasicsForBeginners-/arraysLab1.js
@@ -0,0 +1,20 @@
+SW registered
+Service Worker termination by a timeout timer was canceled because DevTools is attached.
+var baseballTeams = ['Dodgers', 'Giants', 'Mets', 'Yankees', 'Astros'];
+undefined
+baseballTeams[baseballTeams.length] = 'Reds';
+"Reds"
+baseballTeams;
+["Dodgers", "Giants", "Mets", "Yankees", "Astros", "Reds"]
+baseballTeams[1] = 'Butts';
+"Butts"
+baseballTeams;
+["Dodgers", "Butts", "Mets", "Yankees", "Astros", "Reds"]
+var myFavoriteTeam = baseballTeams[0];
+undefined
+myFavoriteTeam;
+"Dodgers"
+baseballTeams.length;
+6
+//challenges
+undefined
